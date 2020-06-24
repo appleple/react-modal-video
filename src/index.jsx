79f763b90b6b709da731bf30185cc0d682ae8d7f
apurvis <apurvis@users.noreply.github.com>
2020-06-24 15:54:47 -0400

    Controls autoplay

--- src/index.jsx
+++ src/index.jsx
@@ -129,7 +129,7 @@ export default class ModalVideo extends React.Component {
                   <div className={this.props.classNames.modalVideoIframeWrap} style={style}>
                     <button className={this.props.classNames.modalVideoCloseBtn} aria-label={this.props.aria.dismissBtnMessage} ref={node => { this.modalbtn = node; }} onKeyDown={this.updateFocus} />
 
-                    <video width='460' height='230' src={this.getVideoUrl(this.props, this.props.videoId)} frameBorder='0' tabIndex='-1' />
+                    <video controls autoplay width='460' height='230' src={this.getVideoUrl(this.props, this.props.videoId)} frameBorder='0' tabIndex='-1' />
                     {/*<iframe width='460' height='230' src={this.getVideoUrl(this.props, this.props.videoId)} frameBorder='0' allowFullScreen={this.props.allowFullScreen} tabIndex='-1' />*/}
                   </div>
                 </div>
